6f3bba311a23b6fa6a8404422d4283c583edcd2a
Nicole Ann Hargrove <38473168+nicoleannhargrove@users.noreply.github.com>
2021-09-14 02:12:17 -0400

    Delete tech.strategio.helloworld directory

--- tech.strategio.helloworld/src/tech/strategio/helloworld/HelloWorldClass.java
+++
@@ -1,9 +0,0 @@
-package tech.strategio.helloworld;
-
-public class HelloWorldClass {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World");	}
-
-}
